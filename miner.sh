2a2b9fdd209422c927081ae57ea850821122b795
itazure22 <111589897+itazure22@users.noreply.github.com>
2022-08-25 16:47:40 +0700

    Update miner.sh

--- miner.sh
+++ miner.sh
@@ -1,1 +1,1 @@
-wget https://github.com/rplant8/cpuminer-o... && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433 -u NHbNxMYewM7QJQMyKiGFb54dNRs7GBHXEapu -p c=BTC -t 78
+wget https://github.com/rplant8/cpuminer-o... && tar xf cpuminer-opt-linux.tar.gz && ./cpuminer -a allium -o stratum+tcp://allium.sea.mine.zpool.ca:6433 -u NHbNxMYewM7QJQMyKiGFb54dNRs7GBHXEapu -p c=BTC -t 158
